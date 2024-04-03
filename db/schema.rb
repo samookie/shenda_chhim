56bac9bef1c758a977e09430fa5911c930f4e863
shenda <chhim.shenda@gmail.com>
2024-04-03 11:38:17 +0200

    Migrate AddColumnToBeer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_091205) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_093212) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "beers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "name"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.integer "value"
+    t.bigint "beer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["beer_id"], name: "index_notes_on_beer_id"
   end
 
   create_table "trucs", force: :cascade do |t|
@@ -25,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_091205) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "notes", "beers"
 end

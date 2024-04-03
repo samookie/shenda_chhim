e2a563464492f356ee0f726abcbb86d294ffaa1f
shenda <chhim.shenda@gmail.com>
2024-04-03 11:13:18 +0200

    Beer

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_152648) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_091205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trucs", force: :cascade do |t|
     t.string "name"

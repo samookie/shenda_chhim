2ccc23e802250918eb386dc8733855753afc7b49
shenda <chhim.shenda@gmail.com>
2024-04-03 09:33:07 +0200

    rollback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :beers
   resources :trucs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_072632) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_02_152648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "beers", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "trucs", force: :cascade do |t|
     t.string "name"
